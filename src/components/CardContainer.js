94214138c2892b8cf238f95ad0d8640449407c82
Lambda_School_Loaner_145 <lambda_school_loaner_145@Lambda-School-Loaner-145s-MacBook-Pro.local>
2019-08-14 13:34:59 -0700

    api working

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 import DatePick from "./DatePick";
+import axios from "axios";
 
 export default function CardContainer() {
-    
-    const [photo, setPhoto] = useState("")
+
+    const [date, setDate] = useState("")
 
     useEffect(() => {
         axios
-            .get("https://api.nasa.gov/planetary/apod")
+            .get(`https://api.nasa.gov/planetary/apod?api_key=BDe7RgqHPuZGj6iDu2KoCvE6WfbDsJb4QLTWHwgx&date=${date}`)
             .then(res => console.log("RES: ", res))
             .catch(err => console.log("OOPS. API ERROR: ", err))
     }, []);
@@ -15,7 +16,7 @@ export default function CardContainer() {
         <div className="card-container">
             <h1>NASA Photo of the Day</h1>
             <br />
-            <DatePick date={date}/>
+            <DatePick />
         </div>
     );
 }
