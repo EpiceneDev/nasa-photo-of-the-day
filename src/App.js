588f76a6a81d147f3e84b5d472724d136d5440da
Lambda_School_Loaner_145 <lambda_school_loaner_145@Lambda-School-Loaner-145s-MacBook-Pro.local>
2019-08-14 13:12:40 -0700

    card built

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import CardContainer from "./components/CardContainer";
 import Card from "./components/Card";
+import DatePick from "./components/DatePick";
 
 function App() {
   return (
--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,12 +1,13 @@
 import React from "react";
 import DatePick from "./DatePick";
 
-export default CardContainer(props) {
+export default function CardContainer(props) {
     let date = props.date;
-    
+
     return (
         <div>
             <h1>NASA Photo of the Day</h1>
+            <br />
             <DatePick date={date}/>
         </div>
     );
